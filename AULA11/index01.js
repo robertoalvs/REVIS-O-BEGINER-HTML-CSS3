0e9f315f68ea094e9db6ab02633ac460eb018b6b
Roberto Alves <robertoalvss@gmail.com>
2024-07-30 11:40:31 -0300

    atualizado js css html

--- AULA11/index01.js
+++ AULA11/index01.js
@@ -1,25 +1,15 @@
-function clicar() {
+function verificar() {
 
-    let nome = document.getElementById('inome')
-    let pais = document.getElementById('ipais')
-    let num = document.getElementById('inasc')
-    let num1 = document.getElementById('iano')
+    let meunome = document.getElementById('inome')
+    let meupais = document.getElementById('ipais')
+    let resposta = document.getElementById('res')
 
-    let n = Number(num.value)
-    let n1 = Number(num1.value)
+    if(meunome == 'Roberto') {
 
-    let soma = n1 - n
+        resposta.innerHTML = 'YES'
+        
+    }else {
 
-    let res = document.getElementById('res')
-
-    if (nome != 'Brasil' || 'brasil') {
-
-        res.innerHTML = `Seu nome e: ${nome}`
-        res.innerHTML += `Você está no ${pais}`
-        res.innerHTML += `Você tem ${soma} de idade`
-
-    } else {
-
-        res.innerHTML = `Vocé é ESTRANGEIRO!`
+        resposta.innerHTML = 'No'
     }
 }
