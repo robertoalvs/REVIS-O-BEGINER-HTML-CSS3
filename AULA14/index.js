07a4637498b39d3866d543ac1eaa041fb7af7ed7
Roberto Alves <robertoalvss@gmail.com>
2024-08-06 14:43:10 -0300

    atualizado

--- AULA14/index.js
+++ AULA14/index.js
@@ -1,19 +1,30 @@
-function consultar() {
+
+function tempo() {
 
     let data = new Date()
     let ano = data.getFullYear()
     let horario = data.getHours()
 
-    let txtnome = document.querySelector('input#txtnome')
-    let txtpais = document.querySelector('input#txtpais')
-
-    let sessao = document.querySelector('secsion#sessao')
-    let imagem = document.querySelector('img#imagem')
-
     let hora = document.querySelector('div#hora')
     let fullano = document.querySelector('div#fullano')
     let foto = document.querySelector('div#foto')
-    let horaeano = document.querySelector('div#horaeano')
+
+    hora.innerHTML = `Agora são: ${horario} horas`
+    fullano.innerHTML = `Estamos em ${ano}`
+}
+
+
+
+function consultar() {
+
+    let txtnome = document.getElementById('txtnome')
+    let txtpais = document.getElementById('txtpais')
+
+    let sessao = document.querySelector('secsion#sessao')
+    let imagem = document.querySelector('img#imagem')
+    let resultado = document.querySelector('div#resultado')
+    let res = document.querySelector('div#res')
+    
 
     // No caso do uso do input radio, usaremos o ByName, por que todos os inputs radio, tem que está com o mesmo nome
     let fsex = document.getElementsByName('radsex')
@@ -26,16 +37,51 @@ function consultar() {
 
     let soma = n2 - n1
 
-    
 
-    if (txtnome.value.length == 0 || txtpais.value.length == 0 || numnasc.value < 1900 || numano.value > ano) {
+    if (txtnome.value.length == 0 || txtpais.value.length == 0 || numnasc.value < 1900 || numano.value > 2024 ) {
 
            alert('ERRO!!! VERIFICAR OS DADOS DIGITADOS')
 
+    } else if (fsex[0].checked) {
+
+        genero = 'HOMEM'
+
+    }  if (soma < 12 ) {
+
+        imagem.src  = './img/bebe-homem.png'
+
+        resultado.innerHTML = `Seu nome é: <strong>${txtnome}</strong> você está em <strong>${txtpais}</strong>`
+
+        res.innerHTML = ` ${genero} com ${soma} anos `
+
+    } else if (soma >= 12 && soma < 18) {
+
+        imagem.src  = './img/crianca-homem.png'
+
+        resultado.innerHTML = `Seu nome é: <strong>${txtnome}</strong> você está em <strong>${txtpais}</strong>`
+
+        res.innerHTML = ` ${genero} com ${soma} anos`
+
+    } else if (soma >= 18 && soma < 60){
+
+        imagem.src  = './img/homem-adulto.png'
+
+        resultado.innerHTML = `Seu nome é: <strong>${txtnome}</strong> você está em <strong>${txtpais}</strong>`
+
+        res.innerHTML = ` ${genero} com ${soma} anos`
+
     } else {
 
-        alert('TUDO OK')
-    }
-    
+        imagem.src  = './img/idosa-homem.png'
+
+        resultado.innerHTML = `Seu nome é: <strong>${txtnome}</strong> você está em <strong>${txtpais}</strong>`
+
+        res.innerHTML = ` ${genero} com ${soma} anos`
+
+
+
+
+
+    } 
      
 }
