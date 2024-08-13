d28397399dad7df4fff5528fb150a0e6b1904b26
Roberto Alves <robertoalvss@gmail.com>
2024-08-13 14:33:41 -0300

    atualizacao while do

---
+++ do-while/index.js
@@ -0,0 +1,19 @@
+
+/*
+while (c <= 10) {
+
+    console.log(`Tentando ${c}`)
+    c++
+} 
+ 
+    do {
+
+        console.log(`Testando ${c}`)
+        c++
+    } while (c <= 10) */
+
+
+    for(let c = 1; c <= 10; c++) {
+
+        console.log(`testando ${c}`)
+    }
